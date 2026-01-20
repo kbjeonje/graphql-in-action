c3576ab64e0ed8d8ea6635a9e2dee0f36e36e9c1
지언 전 <jeonje@kyobobook.com>
2026-01-20 10:26:34 +0900

    Initial commit: GraphQL Study 시작

--- api/src/utils_v1.js
+++ api/src/utils_v1.js
@@ -1,13 +1,9 @@
 import crypto from 'crypto';
 
 export const randomString = (bytesSize = 32) =>
-    crypto.randomBytes(bytesSize).toString('hex');
+  crypto.randomBytes(bytesSize).toString('hex');
 
 export const numbersInRangeObject = (begin, end) => {
-    if (end < begin) {
-        throw new Error(`Invalid range: end (${end}) cannot be less than begin (${begin})`);
-    }
-
     let sum = 0;
     let count = 0;
     for (let i = begin; i <= end; i++) {
